535f325fdabf39c0093bd25df99c33a4f9cbfe84
Mustafizur Rahman <mrahaman59@yahoo.com>
2021-06-18 19:17:58 +0600

    CV

---
+++ CV.java
@@ -0,0 +1,5 @@
+class CV{
+	public static void main(String[] args){
+	System.out.println("Curriculum Vitae-\nName: Mustafizur Rahman\nAddress: Bashabo,Dhaka\nInstitution: American International University Bangladesh\nDepartment: CS\nCourse: Java\nID: 18-36179-1\nSec: M\nCGPA: 3.75");
+	}
+}
