0eda83b61e6b6fa73dc09da0c83ce1cf3ffff79a
Mustafizur Rahman <mrahaman59@yahoo.com>
2021-06-18 19:17:18 +0600

    Array

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Array{	
+public static void main(String[] args){
+		//int[] a= new int[]{1,2,3};
+	      int[] a= {1,2,3};                                          //declaration, instantiation and initialization  
+			
+		/*for(int i:a)                                            //for each loop
+			System.out.println(i);*/
+		
+		/*for(byte i=0;i<3;i++){                                  //for loop
+			System.out.println(a[i]);
+		}*/
+		
+		
+		/*byte i=0;
+		while(i<3){                                              //while loop
+			System.out.println(a[i]);
+			i++;
+		}*/
+		
+		
+		/*byte i=0;                                              //do while loop
+		do{
+			System.out.println(a[i]);
+			i++;
+		}
+		while(i<3);	*/		
+	}
+}
